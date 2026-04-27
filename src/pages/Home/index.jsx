9011d5df58b1e99c57f14b31436f4674da37a8ad
MarianaF3 <marianafernandes.spindola@gmail.com>
2026-04-27 10:54:30 -0300

    Update index.jsx

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,7 +12,7 @@ function Home() {
                 <Link to="/exemplo/1">Exemplo 1 - Componente básico</Link>
                 <Link to="/exemplo/2">Exemplo 2 - Uso de estilização com module</Link>
                 <Link to="/exemplo/3">Exemplo 3 - State </Link>
-                <Link to="/exemplo/4">Exemplo 4 - </Link>
+                <Link to="/exemplo/4">Exemplo 4 - Uso de Componentes </Link>
 
                
 
